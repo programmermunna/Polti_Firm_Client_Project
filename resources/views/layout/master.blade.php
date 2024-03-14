419b5639e2742e7390d28045d0340315ac4bf733
Programmermunna <programmermunna@gmail.com>
2024-03-14 15:55:19 +0600

    hide envalop notificate icon from admin panel

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -283,11 +283,11 @@
                             </li>
 
                             <li role="presentation" class="nav-item dropdown open">
-                                <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1"
+                                {{-- <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1"
                                     data-toggle="dropdown" aria-expanded="false">
                                     <i class="fa fa-envelope-o"></i>
                                     <span class="badge bg-green">6</span>
-                                </a>
+                                </a> --}}
                                 <ul class="dropdown-menu list-unstyled msg_list" role="menu"
                                     aria-labelledby="navbarDropdown1">
                                     <li class="nav-item">
